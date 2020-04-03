6ee58ad96d8642edbdbd033ad5799f1a4669493e
muhammadawais82 <ffor971@gmail.com>
2020-04-03 11:04:53 +0500

    add a new class of js es5 version

--- classes.js
+++ classes.js
@@ -15,3 +15,23 @@ richardsPlane.startEngines();
 
 var jamesPlane = new Plane(4);
 jamesPlane.startEngines();
+
+/////////////////////////////////////////////
+//ES6 example
+class Dessert {
+    constructor(calories = 250) {
+        this.calories = calories;
+    }
+}
+
+class IceCream extends Dessert {
+    constructor(flavor, calories, toppings = []) {
+        super(calories);
+        this.flavor = flavor;
+        this.toppings = toppings;
+    }
+    addTopping(topping) {
+        this.toppings.push(topping);
+    }
+}
+console.log(IceCream)
