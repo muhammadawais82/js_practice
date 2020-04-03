ad36e4a8c95bf2dba40363f286dc4f20debde7d3
muhammadawais82 <ffor971@gmail.com>
2020-04-03 10:51:56 +0500

    add default function parameter in js example

--- pra_this.js
+++ pra_this.js
@@ -22,3 +22,14 @@ function houseDescriptor({houseColor = 'green', shutterColors = ['red']} = {}) {
 console.log(houseDescriptor())
 
 ///////////////////////////////////////////////////////
+//js default function parameter
+function buildHouse({floors=[1], color='red',walls=['brick']} = {}){
+    return `your house has ${floors} floor(s) with ${color} ${walls} walls.`;
+}
+
+console.log(buildHouse()); // Your house has 1 floor(s) with red brick walls.
+console.log(buildHouse({})); // Your house has 1 floor(s) with red brick walls.
+console.log(buildHouse({floors: 3, color: 'yellow'})); // Your house has 3 floor(s) with yellow brick walls.
+
+
+
