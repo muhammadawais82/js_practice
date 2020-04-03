3d388b68786433439417b1f9de4e2f988008b8b8
muhammadawais82 <ffor971@gmail.com>
2020-04-03 11:03:26 +0500

    add a new class example of es5 how class works

---
+++ classes.js
@@ -0,0 +1,17 @@
+//es5 class example
+function Plane(numEngines) {
+    this.numEngines = numEngines;
+    this.enginesActive = false;
+}
+
+// methods "inherited" by all instances
+Plane.prototype.startEngines = function () {
+    console.log('starting engines...');
+    this.enginesActive = true;
+};
+
+var richardsPlane = new Plane(1);
+richardsPlane.startEngines();
+
+var jamesPlane = new Plane(4);
+jamesPlane.startEngines();
