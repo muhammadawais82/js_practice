e12b551bbf260bfaac3d82ee2638005f066456e0
muhammadawais82 <ffor971@gmail.com>
2020-04-03 12:07:58 +0500

    add subclasses in es6 js

--- classes.js
+++ classes.js
@@ -47,6 +47,45 @@ class Plane {
     }
 }
 Plane.badWeather([plane1,plane2,plane3]);
+///////////////////////////////////////////////
+//es6 subclass
+class Tree {
+    constructor(size = '10', leaves = {spring: 'green', summer: 'green', fall: 'orange', winter: null}) {
+        this.size = size;
+        this.leaves = leaves;
+        this.leafColor = null;
+    }
+
+    changeSeason(season) {
+        this.leafColor = this.leaves[season];
+        if (season === 'spring') {
+            this.size += 1;
+        }
+    }
+}
+
+class Maple extends Tree {
+    constructor(syrupQty = 15, size, leaves) {
+        super(size, leaves);
+        this.syrupQty = syrupQty;
+    }
+
+    changeSeason(season) {
+        super.changeSeason(season);
+        if (season === 'spring') {
+            this.syrupQty += 1;
+        }
+    }
+
+    gatherSyrup() {
+        this.syrupQty -= 3;
+    }
+}
+
+const myMaple = new Maple(15, 5);
+myMaple.changeSeason('fall');
+myMaple.gatherSyrup();
+myMaple.changeSeason('spring');
 /////////////////////////////////////////////
 //ES6 example
 class Dessert {
